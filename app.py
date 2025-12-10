bcd12b7d3abe2f32b8fb552cb3064e1985e819f1
solider888 <726280zqlhml@gmail.com>
2025-12-10 17:05:26 -0500

    1

--- app.py
+++ app.py
@@ -1,25 +1,75 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
-import pandas as pd
+import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, dcc, html
+from pages.landing_option_dash import landing_page
+from pages.pricer_option_dash import pricer_page
+from pages.graph_option_dash import graph_page
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
-
-app = Dash()
-
-# Requires Dash 2.17.0 or later
-app.layout = [
-    html.H1(children='Title of Web App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-]
-
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "backgroundColor": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "marginLeft": "18rem",
+    "marginRight": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Option Dash", className="display-4"),
+        html.Hr(),
+        html.P("A dashboard provides intelligence", className="lead"),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Pricer", href="/pricer", active="exact"),
+                dbc.NavLink("Graphs", href="/graphs", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return landing_page
+    elif pathname == "/pricer":
+        return pricer_page
+    elif pathname == "/graphs":
+        return graph_page
+    # If the user tries to reach a different page, return a 404 message
+    return html.Div(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ],
+        className="p-3 bg-light rounded-3",
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5050)
